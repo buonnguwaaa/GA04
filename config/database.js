f7bbfb30368975cd53e5e0c7e8543fd5e16e5e7e
Linh_ASUS <vanlinh042004@gmail.com>
2024-11-06 01:13:44 +0700

    C3

--- config/database.js
+++ config/database.js
@@ -10,3 +10,36 @@ const connect = async () => {
 };
 
 module.exports = { connect };
+
+/*  
+[{
+  "_id": {
+    "$oid": "67299e7af771259390fc140e"
+  },
+  "title": "Essence Mascar",
+  "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+  "price": 9.99,
+  "img": "images/product/nature-zen-3Dn1BZZv3m8-unsplash.jpeg",
+  "catego": "beautifull"
+},
+{
+  "_id": {
+    "$oid": "67299e7af771259390fc140f"
+  },
+  "title": "Eyeshadowor",
+  "description": "The Eyeshadow Palette with Mirror offers a versatile range of eyeshadow shades for creating stunning eye looks. With a built-in mirror, it's convenient for on-the-go makeup application.",
+  "price": 19.99,
+  "img": "images/product/evan-mcdougall-qnh1odlqOmk-unsplash.jpeg",
+  "catego": "be beautiful"
+},
+{
+  "_id": {
+    "$oid": "67299e7af771259390fc1410"
+  },
+  "title": "Powder Canister",
+  "description": "The Powder Canister is a finely milled setting powder designed to set makeup and control shine. With a lightweight and translucent formula, it provides a smooth and matte finish.",
+  "price": 14.99,
+  "img": "images/product/jordan-nix-CkCUvwMXAac-unsplash.jpeg",
+  "catego": "bea bea ddj beautiful"
+}]
+*/
